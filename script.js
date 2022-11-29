377077a5fc9531b157b5b01b45d4c36f6036f72f
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-11-29 14:01:36 +0100

    create event for 'hover', painting grid in one color, and reset button, making grid whole white again

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-// const container = document.querySelector('#window');
 
-// const content = document.createElement('div');
-// const newContent = document.createElement('div');
+const container = document.querySelector('#window');
+const content = document.createElement('div');
+let reset = document.getElementById('reset');
 
-// // content.classList.add('content');
-
-// container.appendChild(content);
-// container.appendChild(newContent);
-
+reset.addEventListener('click', newStart);
 
 //creates a simple grid 16x16 in a given div
 
 function createGrid() {
-    const container = document.querySelector('#window');
-    const content = document.createElement('div');
-
+   
     content.style.width = '28px';
     content.style.height = '28px';
+    content.classList.add('active');
 
     for (var i =0; i < 256; i++) {
         container.appendChild(content.cloneNode());
@@ -24,3 +19,17 @@ function createGrid() {
 }
 
 createGrid();
+
+const smallThings = document.querySelectorAll('.active');
+
+container.addEventListener('mouseover', (event) => {
+    event.target.style.background = "pink";
+} ); 
+
+function newStart(e) {
+    if (e.target.id = 'reset') {
+        for(var i = 0; i < smallThings.length; i++) {
+            smallThings[i].style.background = 'white';
+  }
+ }
+}
