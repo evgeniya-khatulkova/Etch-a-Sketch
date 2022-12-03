1b7c481bb1d37bdd118e97674fb51bf2b2343346
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-03 20:43:54 +0100

    git more drawing modes and add new css style to the  interface

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let reset = document.getElementById('reset');
 let gridSize = document.getElementById('size');
 let discoButton = document.getElementById('disco');
 let blackButton = document.getElementById('black');
-let buttonShaddows = document.getElementById('shaddows'); 
-const greyShades = ["F8F8F8", "D8D8D8", "C0C0C0", "A8A8A8", "909090", "787878", "606060", "505050", "404040", "282828", "000000"];
+let pinkButton = document.getElementById('pink');
+let eraseButton = document.getElementById('erase');
 
 
 
@@ -14,7 +14,8 @@ reset.addEventListener('click', newStart);
 gridSize.addEventListener('click', createGrid);
 discoButton.addEventListener('click',discoColor);
 blackButton.addEventListener('click', blackColor);
-buttonShaddows.addEventListener('click', greyColor);
+pinkButton.addEventListener('click', pinkColor);
+eraseButton.addEventListener('click', eraseColor);
 
 //creates a simple grid 16x16 in a given div
 
@@ -22,9 +23,9 @@ startGrid();
 
 function createGrid(e) {
     if (e.target.id = 'size') {
-    let choice = prompt('Enter your sketch size (from 1 to 99)');
-    if (choice <= 0 || choice > 99){
-        alert("please choose number from 1 to 99");
+    let choice = prompt('Enter your sketch size (from 1 to 100)');
+    if (choice <= 0 || choice > 100){
+        alert("please choose number from 1 to 100");
         return;
     }
     else if (isNaN(choice) == true) {
@@ -90,17 +91,21 @@ function blackColor(e){
 //function for colorful drawing 
 function discoColor(e) {
     if (e.target.id = 'disco') {
+        // const randomColor = Math.floor(Math.random()*16777215).toString(16);
         container.addEventListener('mouseover', (event) => {
             const randomColor = Math.floor(Math.random()*16777215).toString(16);
             event.target.style.background = "#" + randomColor;
     })}}
-//function to work with grey which preogressivly becomes black
-function greyColor(e) {
-    if (e.target.id = 'shaddows') {
-        container.addEventListener('mouseover', (event) => {
-            event.target.style.opacity =+ (0.2);
-        })}}
-        // for(let i = 0; i < greyShades.length; i++) { 
-        // container.addEventListener('mouseover', (event) => {
-        //     event.target.style.background = "#" + greyShades[i];
-        // })}}}
+
+    function pinkColor(e){
+      if (e.target.id = 'pink') {
+            container.addEventListener('mouseover', (event) => {
+                event.target.style.background = "pink";
+            })}}
+
+ function eraseColor(e){
+     if (e.target.id = 'erase') {
+     container.addEventListener('mouseover', (event) => {
+        event.target.style.background = "white";
+   })}}
+        
